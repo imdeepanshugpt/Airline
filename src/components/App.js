dc743021b1cbe089392093f50c957506b889bada
Deepanshu Gupta <M1046873@mindtree.com>
2019-11-29 14:35:11 +0530

    route guard added

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './core/Header';
 import FlightDetails from './core/FlightDetails';
+import { connect } from 'react-redux';
 import { Router, Route } from 'react-router-dom';
 import CheckIn from './features/CheckIn';
 import InFlight from './features/InFlight';
@@ -10,22 +11,36 @@ import history from '../history';
 import Footer from './core/Footer';
 import './App.scss';
 
-const App = () => {
+const App = (props) => {
+    const authenticate = props.isSignedIn;
     return (
         <Router history={history}>
             <div className="wrapper" style={{ fontFamily: 'arial, sans-serif' }}>
                 <Header />
-                <div style={{ minHeight: '100vh' }}>
-                    <Route path="/" exact component={FlightDetails} />
-                    <Route path="/:flightId/checkin" component={CheckIn} />
-                    <Route path="/:flightId/inflight" component={InFlight} />
-                    <Route path="/:flightId/passengerlist" component={PassengerList} />
-                    <Route path="/admin" component={Adminstrator} />
+                <div className="router-box">
+                    {
+                        authenticate ?
+                            <React.Fragment>
+                                <Route path="/" exact component={FlightDetails} />
+                                <Route path="/:flightId/checkin" component={CheckIn} />
+                                <Route path="/:flightId/inflight" component={InFlight} />
+                                <Route path="/:flightId/passengerlist" component={PassengerList} />
+                                <Route path="/admin" component={Adminstrator} />
+                            </React.Fragment>
+                            :
+                            <article className="authenticate">
+                                You need to be authenticated to view ! Please login.
+                            </article>
+                    }
                 </div>
                 <Footer />
             </div>
         </Router>
     );
 }
-
-export default App;
+const mapStateToProps = (state) => {
+    return {
+        isSignedIn: state.auth.isSignedIn
+    };
+}
+export default connect(mapStateToProps, null)(App);

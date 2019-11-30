b5031cc084933f4c0549a43bcec29766b88d3c5c
Deepanshu Gupta <M1046873@mindtree.com>
2019-11-30 12:38:24 +0530

    default service worker removed and custom added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './store/reducers';
 import App from './components/App';
 import reduxThunk from 'redux-thunk';
-import * as serviceWorker from './serviceWorker';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
@@ -19,8 +18,3 @@ ReactDOM.render(
     ,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
